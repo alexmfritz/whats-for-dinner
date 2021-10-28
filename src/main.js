a4f4a9c0b69a8f5f3b4c9e4ba4937bf4efbc1799
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-27 18:08:50 -0700

    Add basic functional buttons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // buttons
 var showFooterButton = document.querySelector('#add-recipe-button');
-var letsCookButton = document.querySelector('lets-cook-button');
-var clearButton = document.querySelector('clear-button');
-var newRecipeButton = document.querySelector('add-new-button');
+var letsCookButton = document.querySelector('.lets-cook-button');
+var clearButton = document.querySelector('.clear-button');
+var newRecipeButton = document.querySelector('.add-new-button');
 // radio input
 var radioSide = document.querySelector('#side');
 var radioMain = document.querySelector('#main');
@@ -18,13 +18,24 @@ var newRecipeName = document.querySelector('#recipe-name');
 
 // event listeners
 showFooterButton.addEventListener('click', viewAddRecipe);
+letsCookButton.addEventListener('click', letsCook);
+clearButton.addEventListener('click', clearMeal);
 
 // event handlers
-
 function viewAddRecipe() {
   if (newRecipeView.classList.contains('hidden')) {
     newRecipeView.classList.remove('hidden');
   } else {
     newRecipeView.classList.add('hidden');
   }
+}
+
+function letsCook() {
+  cookPot.classList.add('hidden');
+  clearButton.classList.remove('hidden');
+}
+
+function clearMeal() {
+  cookPot.classList.remove('hidden');
+  clearButton.classList.add('hidden');
 }
